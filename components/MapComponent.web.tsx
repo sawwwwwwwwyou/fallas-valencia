649d9213b937134f10a6a9ac5ff5a45c2d88e44d
sawwwwwwwwyou <iwillsawyou@proton.me>
2026-01-31 10:51:54 +0100

    tweak: flyTo zoom 14 instead of 15

--- components/MapComponent.web.tsx
+++ components/MapComponent.web.tsx
@@ -121,7 +121,7 @@ export default function WebMapbox({
     if (marker) {
       map.current.flyTo({
         center: [marker.longitude, marker.latitude],
-        zoom: 15,
+        zoom: 14,
         duration: 1000,
       });
     }
@@ -145,7 +145,7 @@ export default function WebMapbox({
         // Fly to marker
         map.current?.flyTo({
           center: [marker.longitude, marker.latitude],
-          zoom: 15,
+          zoom: 14,
           duration: 1000,
         });
       });
